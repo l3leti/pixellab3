ff17139e6d8c66c3fca671df5ffa602ad74bb01d
Leticia_Todero <letitoderi@gmail.com>
2023-10-25 16:24:06 -0300

    spawn

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -2,25 +2,46 @@
 class_name Player
 extends RigidBody2D
 
+## Enums
+enum ESTADO {SPAWN, VIVO, INVENCIBLE, MUERTO}
+
 ## Atributos Export
 export var potencia_motor:int = 20
 export var potencia_rotacion:int = 280
+export var estela_maxima:int = 150
 
 ## Atributos
 var empuje:Vector2 = Vector2 .ZERO
 var dir_rotacion:int = 0
+var estado_actual:int = ESTADO.SPAWN
+var no_hacer_nada = false
 
 ## Atributos Onready
 onready var canion: Canion = $Canion
 onready var laser: RayoLaser = $LaserBeam2D
+onready var estela:Estela = $EstelaPuntoInicio/Trail2D
+onready var motor_sfx:Motor = $MotorSFX
+onready var colisionador:CollisionShape2D = $CollisionShape2D
 
 ## Metodos
 func _unhandled_input(event: InputEvent) -> void:
+	if not esta_input_activo():
+		return
 	#Disparo Rayo
 	if event.is_action_pressed("disparo_secundario"):
 		laser.set_is_casting(true)
 	if event.is_action_released("disparo_secundario"):
 		laser.set_is_casting(false)
+	# Control Estela y sonido motor
+	if event.is_action_pressed("mover_adelante"):
+		estela.set_max_points(estela_maxima)
+		motor_sfx.sonido_on()
+	elif event.is_action_pressed("mover_atras"):
+		estela.set_max_points(0)
+		motor_sfx.sonido_on()
+
+	if (event.is_action_released("mover_adelante") or event.is_action_released("mover_atras")):
+		motor_sfx.sonido_off() 
 
 func _integrate_forces(_state: Physics2DDirectBodyState): 
 	apply_central_impulse(empuje.rotated(rotation))
@@ -29,8 +50,13 @@ func _integrate_forces(_state: Physics2DDirectBodyState):
 func _process(_delta: float): #-> void"# :
 	player_input()
 
+func _ready() -> void:
+	controlador_estados(estado_actual)
+
 ## Metodos Custom
 func player_input(): #(-> void)#:
+	if not esta_input_activo():
+		return
 	#Empuje
 	empuje = Vector2.ZERO
 	if Input.is_action_pressed("mover_adelante"):
@@ -48,4 +74,43 @@ func player_input(): #(-> void)#:
 		canion.set_esta_disparando(true)
 	if Input.is_action_just_released("disparo_principal"):
 		canion.set_esta_disparando(false)
-	
+
+func controlador_estados(nuevo_estado: int) -> void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			colisionador.set_deferred("disabled", false)
+			canion.set_puede_disparar(true)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled", true)
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(true)
+			queue_free()
+		_:
+			printerr("Error de estado")
+	estado_actual = nuevo_estado
+
+func esta_input_activo() -> bool:
+	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
+		return false
+	return true
+
+func esta_funcion_hace_un_monton_de_cosas() -> void:
+	if no_hacer_nada:
+		return
+	#hacer_algo_1()
+	#hacer_algo_2()
+	#hacer_algo_3()
+	print("lo que laburo")
+	#hacer_algo_4()
+	#hacer_algo_5()
+	#var a = 1 * 2 / 10000 * 450 + 78 / 2000
+
+##Señales internas
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name ==  "spawn":
+		controlador_estados(ESTADO.VIVO)
--- Juego/Proyectiles/Canion.gd
+++ Juego/Proyectiles/Canion.gd
@@ -42,6 +42,8 @@ onready var timer_enfriamiento:Timer = $TimerEnfriamiento
 onready var disparo_sfx:AudioStreamPlayer2D = $DisparosSFX
 onready var esta_enfriado:bool = true
 onready var esta_disparando:bool = false 
+onready var puede_disparar:bool = false setget set_puede_disparar
+
 
 ## Atributos
 var puntos_disparo: Array= []
@@ -52,3 +54,6 @@ func set_esta_disparando(disparando: bool) -> void:
 
 func _on_TimerEnfriamiento_timeout() -> void:
 	esta_enfriado = true
+
+func set_puede_disparar(duenio_pude: bool) -> void:
+		puede_disparar = duenio_pude
